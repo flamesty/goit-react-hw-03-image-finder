1783512349069199a12dcbe37083d74b6c79ed83
Vladyslav Sukachev <79707548+flamesty@users.noreply.github.com>
2021-10-14 09:40:36 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,114 +1,111 @@
-import React, { Component } from "react";
-import axios from "axios";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
-
-import Searchbar from "./Searchbar/Searchbar";
-import ImageGallery from "./ImageGallery/ImageGallery";
-import Button from "./Button/Button";
-import Modal from "./Modal/Modal";
-
-const apiKey = "20461350-36527ad634bc0878b1b72e118";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './App.module.css';
+import searchApi from './services/searchApi';
+import SearchForm from './components/Searchbar';
+import ImageGallery from './components/ImageGallery';
+import Button from './components/Button/';
+import Loader from './components/Loader';
+import Modal from './components/Modal';
 
 class App extends Component {
   state = {
-    searchWords: "",
-    images: [],
+    pictures: [],
+    page: 1,
+    query: '',
+    largeImage: '',
+    imgTags: '',
+    error: '',
     showModal: false,
-    modalImage: "",
-    showLoader: false,
-    currentPage: 1,
+    isLoading: false,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.fetchPictures();
+    }
+    if (this.state.page !== 2 && prevState.page !== this.state.page) {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
+      });
+    }
+  }
 
   toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
+    this.setState(state => ({
+      showModal: !state.showModal,
+    }));
   };
 
-  pushImagesToState = (response) => {
-    const imagesFromResponse = response.data.hits;
-    let newSearchArray = [];
-    newSearchArray = [...this.state.images, ...imagesFromResponse];
-    this.setState(({ images }) => ({ images: newSearchArray }));
-  };
-  setModalImage = (linkImg) => {
-    return this.setState(({ modalImage }) => ({ modalImage: linkImg }));
-  };
-  openLargeImage = (linkImg) => {
-    this.setModalImage(linkImg);
+  bigImage = (largeImage = '') => {
+    this.setState({ largeImage });
+
     this.toggleModal();
   };
 
-  loaderToggle = (bool) => {
-    return this.setState(({ showLoader }) => ({ showLoader: bool }));
-  };
+  fetchPictures = () => {
+    const { page, query } = this.state;
 
-  getImages(words, page) {
-    this.loaderToggle(true);
-    axios
-      .get(
-        `https://pixabay.com/api/?q=${words}&page=${page}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=12`
-      )
-      .then((response) => {
-        this.pushImagesToState(response);
-        this.loaderToggle(false);
-        this.setState((prevState) => ({
-          currentPage: prevState.currentPage + 1,
+    const options = {
+      page,
+      query,
+    };
+
+    this.setState({ isLoading: true });
+
+    searchApi(options)
+      .then(pictures => {
+        this.setState(prevState => ({
+          pictures: [...prevState.pictures, ...pictures],
+          page: prevState.page + 1,
         }));
-      });
-  }
-
-  searchFormSubmit = (event) => {
-    event.preventDefault();
-    this.setState({
-      searchWords: "",
-      images: [],
-      showModal: false,
-      modalImage: "",
-      currentPage: 1,
-    });
-    const searchWordsValue = event.target[1].value;
-
-    this.setState({ searchWords: searchWordsValue });
-    const page = 1;
-    this.getImages(searchWordsValue, page);
-    event.target.reset();
+      })
+      .catch(error => this.setState({ error: 'Picture not found' }))
+      .finally(() => this.setState({ isLoading: false }));
   };
-
-  loadMoreFn = () => {
-    this.loaderToggle(true);
-    this.getImages(this.state.searchWords, this.state.currentPage);
+  onChangeQwery = query => {
+    this.setState({ query: query, page: 1, pictures: [], error: null });
   };
 
   render() {
+    const {
+      pictures,
+      isLoading,
+      error,
+      showModal,
+      largeImage,
+      imgTags,
+    } = this.state;
+
     return (
-      <div className="App">
-        {this.state.showModal && (
-          <Modal closeFn={this.toggleModal} loader={this.loaderToggle}>
-            <img src={this.state.modalImage} alt="modal" />
+      <div className={css.App}>
+        <SearchForm onSubmit={this.onChangeQwery} />
+
+        {error && <h1>{error}</h1>}
+
+        <ImageGallery pictures={pictures} bigImage={this.bigImage} />
+        {isLoading && <Loader />}
+        {pictures.length > 11 && !isLoading && (
+          <Button onClick={this.fetchPictures} />
+        )}
+        {showModal && (
+          <Modal showModal={this.bigImage}>
+            <img src={largeImage} alt={imgTags} />
           </Modal>
         )}
-        <Searchbar onSubmit={this.searchFormSubmit} />
-
-        {this.state.searchWords !== "" && (
-          <ImageGallery
-            loader={this.loaderToggle}
-            imagesArray={this.state.images}
-            modalFn={this.openLargeImage}
-          ></ImageGallery>
-        )}
-        {this.state.showLoader && (
-          <Loader
-            className="spin"
-            type="Bars"
-            color="#00BFFF"
-            height={80}
-            width={80}
-          />
-        )}
-        {this.state.searchWords !== "" && <Button fn={this.loadMoreFn} />}
       </div>
     );
   }
 }
-
+App.propTypes = {
+  pictures: PropTypes.array,
+  page: PropTypes.number,
+  query: PropTypes.string,
+  largeImage: PropTypes.string,
+  imgTags: PropTypes.string,
+  error: PropTypes.string,
+  showModal: PropTypes.bool,
+  isLoading: PropTypes.bool,
+};
 export default App;
